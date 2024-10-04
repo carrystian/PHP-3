465a38959cfac04cb09371f469330c266c6e3ce4
carrystian <160556852+carrystian@users.noreply.github.com>
2024-10-04 21:57:44 +0800

    Add files via upload

---
+++ Order.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Menu</h1>
+    <table style="width: 15%;" border="2">
+        <tbody>
+            <tr>
+            <td>Order</td>
+            <td>Amount;</td>
+            </tr>
+            <tr>
+            <td>Burger</td>
+            <td>50</td>
+            </tr>
+            <tr>
+            <td>Fries;</td>
+            <td>75</td>
+            </tr>
+            <tr>
+            <td>Steak</td>
+            <td>150</td>    
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
